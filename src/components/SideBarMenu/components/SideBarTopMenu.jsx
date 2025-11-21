4d0628815c8e9ebe5e8f2e9df9e2dd062d407b0e
Aman khare <WbstAman>
2025-11-21 18:55:00 +0530

    HOC icon

--- src/components/SideBarMenu/components/SideBarTopMenu.jsx
+++ src/components/SideBarMenu/components/SideBarTopMenu.jsx
@@ -310,7 +310,7 @@ const SideBarTopMenu = ({ isOpen }) => {
 export default SideBarTopMenu;
 
 
-const CasinoIcon = ({ size = 30, color = "#fff" }) => {
+function CasinoIcon  ({ size = 30, color = "#fff" }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg"
       width={size}
